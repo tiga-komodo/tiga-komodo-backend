3b13d57c3241ad985074fbed82e0840b5be1a12c
zakintaliban <nukmans23@gmail.com>
2019-12-12 12:24:19 +0700

    nambah controllers dan index

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require("body-parser");
 var cors = require("cors");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var usersRouter = require("./routes/user");
 
 var app = express();
 
