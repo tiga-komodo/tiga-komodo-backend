bcc5318a1be2b302bfbdaf14d58dfcbba9ce0566
iqbalelyas <iqbal35mm@gmail.com>
2019-12-16 15:56:32 +0700

    change url

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI =
+  process.env.MONGODB_URI || `mongodb://localhost:27017/olshop_db`;
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
