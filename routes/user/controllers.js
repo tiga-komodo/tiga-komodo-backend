0de70c209806bc67f0e67ceb1e4daa165c9bcf1f
iqbalelyas <iqbal35mm@gmail.com>
2019-12-19 14:15:24 +0700

    min change

--- routes/user/controllers.js
+++ routes/user/controllers.js
@@ -93,7 +93,7 @@ module.exports = {
                <br />
                <br />
                <b>Salam</b>
-               <p>Serah.com</p>
+               <p>Timepiece.com</p>
         `
       });
     }
